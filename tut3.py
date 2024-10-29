6ea531d68f4b28865c62368962c120d3aba7dda4
Vivek Kumar Mishra(308083) <vivek571@gmail.com>
2024-10-29 18:29:27 +0530

    dataframe

---
+++ tut3.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# df=pd.read_csv(r"C:\Users\God\Downloads\PythonPrac\Jupyter\marks.csv")
+# print(df.columns)
+# print(df.rpow)
+
+# print(df.shape)
+# print(df.size)
+
+# print(df.head())
+# print(df.head(2))
+
+# print(df.tail())
+# print(df.tail(3))
+
+# print(df.describe())
+
+# print(df.info())
+
+df1=pd.read_csv(r'C:\Users\God\Downloads\PythonPrac\Jupyter\Restaurant.csv')
+# print(df1.shape)
+# print(df1.size)
+
+# print(df1.head())
+# print(df1.info())
+# print(df1.describe())
+
+# print(df1.columns)
+# print(df1.rpow)
+# print(df1.size)
+# print(df1.tail(2))
+print(df1.tail(0))
+print(df1.columns)
